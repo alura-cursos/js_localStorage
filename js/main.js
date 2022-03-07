a6b2d96d0d229a4e7f19845bc189122b5dcf93f1
luanalvesdev <lluan.alves1@gmail.com>
2022-03-07 15:48:20 -0300

    aula_2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
-const form = document.getElementById("novoItem")
+const form = document.getElementById("novoItem") 
 const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-form.addEventListener("submit", (evento) => {
+itens.forEach( (elemento) => {    
+    criaElemento(elemento)
+} )
+
+form.addEventListener("submit", (evento) => {  
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    const itemAtual = {
+    "nome": nome.value,
+    "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {  
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-
+    numeroItem.innerHTML = item.quantidade
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
 }

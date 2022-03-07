a6f532d6e3347e5799aa513e7dc22d5f2e5c47dc
luanalvesdev <lluan.alves1@gmail.com>
2022-03-07 16:29:49 -0300

    aula_4

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
-const form = document.getElementById("novoItem") 
+const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []  
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach( (elemento) => {    
+itens.forEach( (elemento) => {
     criaElemento(elemento)
 } )
 
-form.addEventListener("submit", (evento) => {  
+form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
     const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
 
-    itens.push(itemAtual)
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -27,15 +39,20 @@ form.addEventListener("submit", (evento) => {
     quantidade.value = ""
 })
 
-function criaElemento(item) {  
-    const novoItem = document.createElement('li')
+function criaElemento(item) {
+    const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong')
+    const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
-
+    
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
